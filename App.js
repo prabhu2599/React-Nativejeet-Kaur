96833110c6488e1e21d1b6ab96e314c16808b4ea
prabhu2599 <js9884798839@gmai.com>
2020-10-02 09:02:45 +0530

    Added project

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState ('pg');
+  const [person, setPerson] = useState({name: 'mario', age: '40'})
+  const ifclicked = () => {
+    setName('Prabhu ji')
+    setPerson({name:'hanji', age: '49'})
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Purple}>My name is {person.name} and age is {person.age}</Text>
+      <Text></Text>
       <StatusBar style="auto" />
+      <View style = {styles.buttonContainer}>
+        <Button title="Update State" onPress={ifclicked}/>
+      </View>
     </View>
   );
 }
@@ -14,8 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  Purple: {    
+    color: '#9C27B0',   
+  },
+  buttonContainer: {
+    marginTop: 20
+  }
+})
